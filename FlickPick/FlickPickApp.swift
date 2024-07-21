3fa28abf4124b1605ae4fcc4914e65fcceb4ff7a
Saumya Singh <saumya0309singh@gmail.com>
2024-07-21 15:57:25 +0530

    Initial Commit

---
+++ FlickPick/FlickPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickPickApp.swift
+//  FlickPick
+//
+//  Created by Saumya Singh on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
